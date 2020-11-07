f4bb56cc7029bdde3355b3875617e6b18374703d
Alexandra Vezendan <alexanndra.vezendan@yahoo.ro>
2020-11-07 19:20:12 +0200

    new commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!!!!!!! I'm ale");
+        System.out.println("Que pasa aqui?");
+
+    }
+}
