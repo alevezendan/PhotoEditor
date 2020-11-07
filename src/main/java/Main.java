ead05031d11ccd9ebbc53b3f839254a281d7d7f8
Alexandra Vezendan <alexanndra.vezendan@yahoo.ro>
2020-11-07 21:24:29 +0200

    incerc

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,15 @@
+import javax.swing.*;
 
-
-public class Main {
+public class Main{
 
     public static void main(String[] args) {
         System.out.println("Hello world!!!!!!! I'm ale");
         System.out.println("Que pasa aqui?");
         System.out.println("Yasli");
+
+        View v=new View("MyApp",400,400);
+        //JFrame mainFrame=new View("MyApp",400,400);
+        //mainFrame.setVisible(true);
+       // v.setVisible(true);
     }
 }
--- src/main/java/View.java
+++ src/main/java/View.java
@@ -1,2 +1,46 @@
-public class View {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class View extends JFrame{
+    private JFrame frame;
+    private JPanel mainPanel;
+    private JMenuBar menuBar;
+    private JMenu menu1,menu2,menu3;
+    private JMenuItem i1,i2,i3;
+
+    public View(String s,int x,int y) {
+        frame=new JFrame(s);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(x,y);
+        frame.setContentPane(mainPanel);
+        frame.setVisible(true);
+        mainPanel= new JPanel();
+        //mainPanel.setLayout(new BoxLayout(mainPanel,BoxLayout.Y_AXIS));
+        mainPanel.setLayout(new FlowLayout());
+
+        menuBar=new JMenuBar();
+        mainPanel.add(menuBar);
+        menu1=new JMenu("File");
+        i1=new JMenuItem("Add Image");
+        i2=new JMenuItem("Open Image");
+        i3= new JMenuItem("Recents");
+        mainPanel.add(menuBar);
+        menuBar.add(menu1);
+        //menuBar.add(menu2);
+       // menuBar.add(menu3);
+        menu1.add(i1);
+        menu1.add(i2);
+        menu1.add(i3);
+
+
+
+
+
+       // frame.pack();
+
+
+    }
+
 }
