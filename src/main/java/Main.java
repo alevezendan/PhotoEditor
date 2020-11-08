a837b48c12a730df5c9b95bcbd0b847041985e56
Alexandra Vezendan <alexanndra.vezendan@yahoo.ro>
2020-11-08 19:14:31 +0200

    nu inteleg

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,8 @@ public class Main{
         System.out.println("Hello world!!!!!!! I'm ale");
         System.out.println("Que pasa aqui?");
         System.out.println("Yo como manzanas.");
+        System.out.println("yo no!!!");
+        System.out.println("No entiendo!");
 
         View v=new View("MyApp",400,400);
         //JFrame mainFrame=new View("MyApp",400,400);
