1513de793925d72afc95ed45147072e7d87fc56b
kyokeen <petreairina60@gmail.com>
2020-11-08 21:46:37 +0200

    working gui and packages 2

--- src/main/java/GUI/EditorGUI.java
+++ src/main/java/GUI/EditorGUI.java
@@ -18,15 +18,44 @@ public class EditorGUI extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage stage) {
+        /*
         Parent root = null;
         try {
-            root = new FXMLLoader(this.getClass().getResource("gui_test.fxml")).load();
+            root = new FXMLLoader(this.getClass().getResource("GUI//gui_test.fxml")).load();
         } catch (IOException e) {
             e.printStackTrace();
         }
         primaryStage.setTitle("Dice Roller");
         primaryStage.setScene(new Scene(root));
-        primaryStage.show();
+        primaryStage.show();*/
+
+        // Create the FXMLLoader
+        FXMLLoader loader = new FXMLLoader();
+        // Path to the FXML File
+        String fxmlDocPath = "X:\\faculta\\year 3\\IS\\PhotoEditor\\PhotoEditor\\src\\main\\java\\GUI\\gui_test.fxml";
+        FileInputStream fxmlStream = null;
+        try {
+            fxmlStream = new FileInputStream(fxmlDocPath);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // Create the Pane and all Details
+        VBox root = null;
+        try {
+            root = (VBox) loader.load(fxmlStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Create the Scene
+        Scene scene = new Scene(root);
+        // Set the Scene to the Stage
+        stage.setScene(scene);
+        // Set the Title to the Stage
+        stage.setTitle("A simple FXML Example");
+        // Display the Stage
+        stage.show();
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
-module PhotoEditor {
+module mod {
     requires javafx.controls;
     requires javafx.graphics;
+    requires javafx.fxml;
+    requires java.desktop;
 
     opens GUI;
 }
